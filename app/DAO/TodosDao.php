381a18a37e3727c989b303f90a35d6d69c04bdb7
Andre Morganti <acmoragnti@gmail.com>
2022-07-12 07:58:34 -0300

    implementing namespaces

--- app/DAO/TodosDao.php
+++ app/DAO/TodosDao.php
@@ -1,67 +1,73 @@
 <?php
 
+namespace App\TodosDao;
+
+use \PDO;
+use App\TodosModel;
+
 class TodosDAO
 {
-    private $conn;
+  private $conn;
 
-    public function __construct()
-    {
-        $dsn = "mysql:host=mysql_db:3306;dbname=todos_db";
-        $this->conn = new PDO($dsn, 'root', 'root');
-    }
+  public function __construct()
+  {
+    $dsn = "mysql:host=mysql_db:3306;dbname=todos_db";
+    $this->conn = new PDO($dsn, 'root', 'root');
+  }
 
-    public function insert(TodosModel $model)
-    {
-        $sql = "INSERT INTO todos (name, status, creation_date) VALUES (?, ?, ?)";
+  public function insert(TodosModel $model)
+  {
+    $sql = "INSERT INTO todos (name, status, creation_date) VALUES (?, ?, ?)";
 
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(1, $model->name);
-        $stmt->bindValue(2, $model->status);
-        $stmt->bindValue(3, $model->creation_date);
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(1, $model->name);
+    $stmt->bindValue(2, $model->status);
+    $stmt->bindValue(3, $model->creation_date);
 
-        $stmt->execute();
-    }
+    $stmt->execute();
+  }
 
-    public function update(TodosModel $model)
-    {
-        $sql = "UPDATE todos set name=?, status=?, creation_date=? WHERE id = ?";
+  public function update(TodosModel $model)
+  {
+    $sql = "UPDATE todos set name=?, status=?, creation_date=? WHERE id = ?";
 
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(1, $model->name);
-        $stmt->bindValue(2, $model->status);
-        $stmt->bindValue(3, $model->creation_date);
-        $stmt->bindValue(4, $model->id);
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(1, $model->name);
+    $stmt->bindValue(2, $model->status);
+    $stmt->bindValue(3, $model->creation_date);
+    $stmt->bindValue(4, $model->id);
 
-        $stmt->execute();
-    }
+    $stmt->execute();
+  }
 
-    public function select()
-    {
-        $sql = "SELECT * FROM todos";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
+  public function select()
+  {
+    $sql = "SELECT * FROM todos";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute();
 
-        return $stmt->fetchAll(PDO::FETCH_CLASS);
-    }
+    return $stmt->fetchAll(PDO::FETCH_CLASS);
+  }
 
-    public function selectById($id)
-    {
-        include_once "models/TodosModel.php";
-        $sql = "SELECT * FROM todos WHERE id = ? LIMIT 1";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(1, $id);
-        $stmt->execute();
+  public function selectById($id)
+  {
+    include_once "models/TodosModel.php";
+    $sql = "SELECT * FROM todos WHERE id = ? LIMIT 1";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(1, $id);
+    $stmt->execute();
 
-        return $stmt->fetchObject("TodosModel");        
-    }
+    return $stmt->fetchObject("TodosModel");
+  }
 
-    public function delete(int $id)
-    {
+  public function delete(int $id)
+  {
 
-        $sql = "DELETE FROM todos WHERE id = ?";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(1, $id);
+    $sql = "DELETE FROM todos WHERE id = ?";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(1, $id);
 
-        $stmt->execute();
-    }
+    $stmt->execute();
+  }
 }
+
--- app/models/TodosModel.php
+++ app/models/TodosModel.php
@@ -1,53 +1,56 @@
 <?php
 
+namespace App\TodosModel;
+
+use App\TodosDao;
+
 class TodosModel
 {
-    public $id = '';
-    public $name = '';
-    public $status = '';
-    public $creation_date = '';
+  public $id = '';
+  public $name = '';
+  public $status = '';
+  public $creation_date = '';
 
-    public $rows, $row;
+  public $rows, $row;
 
-    public function save()
-    {
-        include 'DAO/TodosDao.php';
+  public function save()
+  {
+    //include 'DAO/TodosDao.php';
 
-        $dao = new TodosDao();
+    $dao = new TodosDao();
 
-        if ($this->id != ''){
-            $dao->update($this);
-        } else {
-            $dao->insert($this);
-        }
-
+    if ($this->id != '') {
+      $dao->update($this);
+    } else {
+      $dao->insert($this);
     }
+  }
 
-    public function getAllRows()
-    {
-        include 'DAO/TodosDao.php';
+  public function getAllRows()
+  {
+    //include 'DAO/TodosDao.php';
 
-        $dao = new TodosDao();
+    $dao = new TodosDao();
 
-        $this->rows = $dao->select();
-    }
+    $this->rows = $dao->select();
+  }
 
-    public function getById(int $id)
-    {
-        include 'DAO/TodosDao.php';
+  public function getById(int $id)
+  {
+    //include 'DAO/TodosDao.php';
 
-        $dao = new TodosDao();
+    $dao = new TodosDao();
 
-        return $dao->selectById($id);
+    return $dao->selectById($id);
+  }
 
-    }
+  public function delete(int $id)
+  {
+    //include 'DAO/TodosDao.php';
 
-    public function delete(int $id)
-    {
-        include 'DAO/TodosDao.php';
+    $dao = new TodosDao();
 
-        $dao = new TodosDao();
-
-        $dao->delete($id);
-    }
+    $dao->delete($id);
+  }
 }
+
